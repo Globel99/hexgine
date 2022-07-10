ea313dfd7498a1bb5246acf21c2ef1d7fd3d45e9
Globel99 <kap.levente@gmail.com>
2022-07-10 22:52:38 +0200

    instanced rendering works

--- src/main.ts
+++ src/main.ts
@@ -5,37 +5,45 @@ import './gui/camera-gui';
 import { Hexagon } from './geometry/hexagon';
 
 const geometry = new Hexagon();
-const material = new Three.MeshNormalMaterial();
+const material = new Three.MeshBasicMaterial();
 
 const hex = new Three.Mesh(geometry, material);
 
-const count = 20;
-const hexes = new Three.InstancedMesh(geometry, material, count);
+const rows = 10;
+const columns = 10;
 
-let inRow = 0;
-let rowCounter = 1;
-const perRow = 5;
-new Array(count).fill([]).forEach((v, i) => {
-  inRow++;
-  if (inRow > perRow) {
-    rowCounter++;
-    inRow = 1;
+const hexes = new Three.InstancedMesh(geometry, material, rows * columns);
+
+const SIDE = 1;
+const A = Math.sqrt(3) / 2;
+const d = 0.2;
+const EVENODDDIFF = A + d / 2;
+
+const xD = 2 * A + d;
+const yD = (3 * SIDE) / 2 + d * A;
+
+let i = 0;
+
+for (let r = 0; r < rows; r++) {
+  for (let c = 0; c < columns; c++) {
+    const evenD = r % 2 ? EVENODDDIFF : 0;
+    const x = c * xD + evenD;
+    const y = r * yD;
+
+    console.log({ x, y });
+
+    const matrix = new Three.Matrix4();
+    matrix.setPosition(x, 1, y);
+
+    hexes.setMatrixAt(i, matrix);
+
+    i += 1;
+    const color = 1 / (255 / i);
+    hexes.setColorAt(i, new Three.Color(color, color, color));
   }
-  const zDistance = 1.6;
-  const xDistance = 1.8;
-  const p = 10;
-  const matrix = new Three.Matrix4();
-  console.log(rowCounter);
-  matrix.setPosition(
-    i * xDistance - rowCounter * 8.1 - (rowCounter === 3 ? 2 : 0),
-    0,
-    rowCounter * zDistance,
-  );
+}
 
-  hexes.setMatrixAt(i, matrix);
-});
-
-camera.position.set(0, 50, 0);
+camera.position.set(30, 20, 0);
 camera.lookAt(scene.position);
 
 scene.add(hexes);
