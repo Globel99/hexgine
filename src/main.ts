e7923202aa6cf4b2d451ae8ae917004e7e2aa153
Globel99 <kap.levente@gmail.com>
2022-07-17 19:18:08 +0200

    added camera control package

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import './gui/camera-gui';
+import { OrbitControls } from '@three-ts/orbit-controls';
 
 import RenderLoop from './render-loop';
-import { scene, camera } from './base';
+import { scene, camera, renderer } from './base';
 import Chunk from './chunk';
 
-const chunk = new Chunk(100, 100);
+const chunk = new Chunk(10, 10);
 
-camera.position.set(30, 20, 0);
+camera.position.set(60, 50, 0);
 camera.lookAt(scene.position);
 
 scene.add(chunk);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const renderLoop = new RenderLoop();
 renderLoop.start();
