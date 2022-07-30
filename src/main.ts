a11d9ccbcd7a0917b0c9b0d5cc3265f2d8385c06
Globel99 <kap.levente@gmail.com>
2022-07-30 22:01:07 +0200

    HEX-9 added light

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { OrbitControls } from '@three-ts/orbit-controls';
-import { Color } from 'three';
+import { DirectionalLight } from 'three';
 
 import './gui/camera-gui';
-import gui from './gui/camera-gui';
 
 import RenderLoop from './render-loop';
 import { scene, camera, renderer } from './base';
 import Map from './group/map';
 
-const map = new Map(3, 3, 10);
+const map = new Map(10, 10, 20);
 
 camera.position.set(60, 50, 0);
 
+const light = new DirectionalLight(0xffffff, 1.5);
+light.position.set(50, 20, 50);
+light.target.position.set(1000, 0, 1000);
+light.castShadow = true;
+
 scene.add(map);
+scene.add(light);
 camera.lookAt(scene.position);
 
 /* gui.add(
