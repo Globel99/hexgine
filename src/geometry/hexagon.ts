e33d992a7eff5b7252e28837b52a06f6eea378b6
Globel99 <kap.levente@gmail.com>
2022-07-30 21:58:05 +0200

    adjust hexagon

--- src/geometry/hexagon.ts
+++ src/geometry/hexagon.ts
@@ -5,12 +5,12 @@ export class Hexagon extends CylinderGeometry {
   triangleHeight: number;
   gap: number;
 
-  constructor(height = 0.1) {
+  constructor(height = 0.05) {
     super(1, 1, height, 6, 1);
 
     this.sideLength = 1;
     this.triangleHeight = Math.sqrt(3) / 2;
-    this.gap = 0.2;
+    this.gap = 0.1;
   }
 
   get rowDistance(): number {
